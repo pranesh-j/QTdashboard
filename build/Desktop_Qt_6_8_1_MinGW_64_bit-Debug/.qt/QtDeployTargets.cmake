40a902386fbcda88f091ad052b527ccba3706975
Pranesh Jahagirdar <44323581+pranesh-j@users.noreply.github.com>
2025-01-14 20:10:20 +0530

    Delete build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_appdashboard_FILE C:/Users/pooja/OneDrive/Desktop/dashboard/dashboard/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/appdashboard.exe)
-set(__QT_DEPLOY_TARGET_appdashboard_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_appdashboard_RUNTIME_DLLS C:/Qtapp/6.8.1/mingw_64/bin/Qt6QuickControls2.dll;C:/Qtapp/6.8.1/mingw_64/bin/Qt6QuickTemplates2.dll;C:/Qtapp/6.8.1/mingw_64/bin/Qt6Quick.dll;C:/Qtapp/6.8.1/mingw_64/bin/Qt6QmlMeta.dll;C:/Qtapp/6.8.1/mingw_64/bin/Qt6QmlWorkerScript.dll;C:/Qtapp/6.8.1/mingw_64/bin/Qt6OpenGL.dll;C:/Qtapp/6.8.1/mingw_64/bin/Qt6Gui.dll;C:/Qtapp/6.8.1/mingw_64/bin/Qt6QmlModels.dll;C:/Qtapp/6.8.1/mingw_64/bin/Qt6Qml.dll;C:/Qtapp/6.8.1/mingw_64/bin/Qt6Network.dll;C:/Qtapp/6.8.1/mingw_64/bin/Qt6Core.dll)
--- build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/.qt/bin/qt_setup_tool_path.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set PATH=C:\Qtapp\6.8.1\mingw_64\bin;%PATH%
-%*
